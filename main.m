aa96237faaab2cbd44d74cdd53499fca1a6b29f0
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 17:22:24 -0700

    Create main.m

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  candycrush
+//
+//  Created by Pragya Singh on 3/27/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
