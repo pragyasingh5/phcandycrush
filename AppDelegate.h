b478eb2ece6c1bb061fce0f52116b694d8cb13b9
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 17:18:47 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  candycrush
+//
+//  Created by Pragya Singh on 3/27/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
